f206a4f53800f7424655425663d53af9110b62ba
81516741 <lingda@lingdas-MacBook-Pro.local>
2018-02-04 21:34:24 +0800

    提交无限循环第三方

--- Example/LDCycleView/LDViewController.m
+++ Example/LDCycleView/LDViewController.m
@@ -8,22 +8,33 @@
 
 #import "LDViewController.h"
 
+#import "HRCycleView.h"
+
 @interface LDViewController ()
 
 @end
 
 @implementation LDViewController
 
-- (void)viewDidLoad
-{
+- (void)viewDidLoad {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    NSArray * colors = @[[UIColor redColor],[UIColor greenColor],[UIColor blackColor]];
+    HRCycleView * view = [HRCycleView cycleView:2];
+    view.frame = CGRectMake(0, 64, [UIScreen mainScreen].bounds.size.width, 150);
+    view.itemCount = ^(){
+        return (NSInteger)3;
+    };
+    view.fetchItem = ^(NSInteger index,UIView * customView){
+        if (customView == nil) {
+            customView = [[UIView alloc]init];
+        }
+        customView.backgroundColor = colors[index];
+        NSLog(@"%p",customView);
+        return customView;
+    };
+    view.showIndicator = ^{
+        return YES;
+    };
+    [self.view addSubview:view];
 }
-
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
-
 @end
---
+++ Example/Pods/Target Support Files/LDCycleView/LDCycleView-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_LDCycleView : NSObject
+@end
+@implementation PodsDummy_LDCycleView
+@end
