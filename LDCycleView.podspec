00d0774c766ef4c8352e0b9f1e08c6d236a6bcc2
81516741 <lingda@lingdas-MacBook-Pro.local>
2018-02-04 21:36:07 +0800

    提交无限循环

--- LDCycleView.podspec
+++ LDCycleView.podspec
@@ -9,7 +9,7 @@
 Pod::Spec.new do |s|
   s.name             = 'LDCycleView'
   s.version          = '0.1.0'
-  s.summary          = 'A short description of LDCycleView.'
+  s.summary          = '无限循环'
 
 # This description is used to generate tags and improve search results.
 #   * Think: What does it do? Why did you write it? What is the focus?
@@ -18,14 +18,14 @@ Pod::Spec.new do |s|
 #   * Finally, don't worry about the indent, CocoaPods strips it!
 
   s.description      = <<-DESC
-TODO: Add long description of the pod here.
+    超级轻量级的无限循环
                        DESC
 
-  s.homepage         = 'https://github.com/81516741@qq.com/LDCycleView'
+  s.homepage         = 'https://github.com/81516741/LDCycleView'
   # s.screenshots     = 'www.example.com/screenshots_1', 'www.example.com/screenshots_2'
   s.license          = { :type => 'MIT', :file => 'LICENSE' }
   s.author           = { '81516741@qq.com' => '81516741@qq.com' }
-  s.source           = { :git => 'https://github.com/81516741@qq.com/LDCycleView.git', :tag => s.version.to_s }
+  s.source           = { :git => 'https://github.com/81516741/LDCycleView.git', :tag => s.version.to_s }
   # s.social_media_url = 'https://twitter.com/<TWITTER_USERNAME>'
 
   s.ios.deployment_target = '8.0'
---
+++ LDCycleView/Classes/HRCycleView.m
@@ -0,0 +1,196 @@
+//
+//  HRCyleView.m
+//  无限滚动
+//
+//  Created by ld on 17/2/24.
+//  Copyright © 2017年 ld. All rights reserved.
+//
+
+#import "HRCycleView.h"
+#import <objc/runtime.h>
+
+#define kReuseID @"HRCycleView"
+#define kRepeatCount 1000
+
+@interface HRCycleView ()
+
+@property (weak, nonatomic) IBOutlet UICollectionView *collectionView;
+@property (weak, nonatomic) IBOutlet UICollectionViewFlowLayout *flowLayout;
+@property (weak, nonatomic) IBOutlet UIPageControl *pageController;
+@property (assign ,nonatomic) NSInteger currentIndex;
+@property (nonatomic,strong) NSTimer * timer;
+@property (assign ,nonatomic) NSTimeInterval duration;
+
+@end
+
+@implementation HRCycleView
+
++ (instancetype)cycleView:(NSTimeInterval)duration
+{
+    HRCycleView * cycleView = [[[NSBundle bundleForClass:self] loadNibNamed:@"HRCycleView" owner:nil options:nil] lastObject];
+    [cycleView.collectionView registerClass:[UICollectionViewCell class] forCellWithReuseIdentifier:kReuseID];
+    cycleView.duration = duration;
+    cycleView.pageController.enabled = NO;
+    [cycleView startTimer];
+    return cycleView;
+}
+#pragma mark 布局UI
+- (void)setFrame:(CGRect)frame
+{
+    [super setFrame:frame];
+    self.flowLayout.itemSize = self.frame.size;
+}
+
+- (void)layoutSubviews
+{
+    [super layoutSubviews];
+    _pageController.hidden = _showIndicator ? !_showIndicator() : YES;
+    _pageController.numberOfPages = self.itemCount();
+    _collectionView.contentOffset = CGPointMake(self.flowLayout.itemSize.width * (kRepeatCount / 2) * self.itemCount(), 0);
+    _collectionView.contentInset = UIEdgeInsetsMake(0, 0, 0, 0);
+}
+
+#pragma mark 定时器相关
+- (void)startTimer
+{
+    _timer = [NSTimer scheduledTimerWithTimeInterval:_duration  target:[hr_YYTextWeakProxy proxyWithTarget:self] selector:@selector(autoScroll) userInfo:nil repeats:YES];
+}
+
+- (void)invalidateTimer
+{
+    [_timer invalidate];
+}
+
+- (void)autoScroll
+{
+    //如果item的个数为0 则不需要滚动item了 并暂停定时器
+    if (self.itemCount() == 0) {
+        [self invalidateTimer];
+        return;
+    }
+    //因为定位到第一个，当用户真的拖到尽头了程序会蹦
+    if ((_currentIndex + 1) % self.itemCount() == 0) {
+        NSInteger desIndex = (kRepeatCount / 2 + 1) * self.itemCount() - 1;
+        _collectionView.contentOffset = CGPointMake(self.flowLayout.itemSize.width * desIndex, 0);
+    }
+    NSIndexPath * indexPath = [NSIndexPath indexPathForItem:_currentIndex + 1 inSection:0];
+    [self.collectionView scrollToItemAtIndexPath:indexPath atScrollPosition:UICollectionViewScrollPositionLeft animated:YES];
+}
+
+#pragma mark collectionView delegate 代理
+- (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section
+{
+    return self.itemCount() * kRepeatCount;
+}
+
+- (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath
+{
+    UICollectionViewCell * cell = [collectionView dequeueReusableCellWithReuseIdentifier:kReuseID forIndexPath:indexPath];
+    UIView * customView = self.fetchItem(indexPath.row % self.itemCount(),objc_getAssociatedObject(cell, _cmd));
+    customView.frame = cell.bounds;
+    objc_setAssociatedObject(cell, _cmd, customView, OBJC_ASSOCIATION_RETAIN);
+    [cell addSubview:customView];
+    return cell;
+}
+
+#pragma mark scrollView delegate 代理
+
+
+
+- (void)scrollViewDidScroll:(UIScrollView *)scrollView
+{
+    //如果没有item 则直接返回
+    if (self.itemCount() == 0) {
+        return;
+    }
+    
+    //控制指示点的位置
+    _currentIndex = (NSInteger)(scrollView.contentOffset.x/scrollView.bounds.size.width + 0.5);
+    NSInteger  pageNum = _currentIndex % self.itemCount();
+    self.pageController.currentPage = pageNum;
+}
+
+
+- (void)scrollViewWillBeginDragging:(UIScrollView *)scrollView
+{
+    [self invalidateTimer];
+}
+
+- (void)scrollViewDidEndDragging:(UIScrollView *)scrollView willDecelerate:(BOOL)decelerate
+{
+    [self startTimer];
+}
+
+@end
+
+#pragma mark - 其他类
+@implementation hr_YYTextWeakProxy
+
+- (instancetype)initWithTarget:(id)target {
+    _target = target;
+    return self;
+}
+
++ (instancetype)proxyWithTarget:(id)target {
+    return [[hr_YYTextWeakProxy alloc] initWithTarget:target];
+}
+
+- (id)forwardingTargetForSelector:(SEL)selector {
+    return _target;
+}
+
+- (void)forwardInvocation:(NSInvocation *)invocation {
+    void *null = NULL;
+    [invocation setReturnValue:&null];
+}
+
+- (NSMethodSignature *)methodSignatureForSelector:(SEL)selector {
+    return [NSObject instanceMethodSignatureForSelector:@selector(init)];
+}
+
+- (BOOL)respondsToSelector:(SEL)aSelector {
+    return [_target respondsToSelector:aSelector];
+}
+
+- (BOOL)isEqual:(id)object {
+    return [_target isEqual:object];
+}
+
+- (NSUInteger)hash {
+    return [_target hash];
+}
+
+- (Class)superclass {
+    return [_target superclass];
+}
+
+- (Class)class {
+    return [_target class];
+}
+
+- (BOOL)isKindOfClass:(Class)aClass {
+    return [_target isKindOfClass:aClass];
+}
+
+- (BOOL)isMemberOfClass:(Class)aClass {
+    return [_target isMemberOfClass:aClass];
+}
+
+- (BOOL)conformsToProtocol:(Protocol *)aProtocol {
+    return [_target conformsToProtocol:aProtocol];
+}
+
+- (BOOL)isProxy {
+    return YES;
+}
+
+- (NSString *)description {
+    return [_target description];
+}
+
+- (NSString *)debugDescription {
+    return [_target debugDescription];
+}
+
+@end
+
